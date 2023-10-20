2979f8973eb38d05b9d4dbd6d27d23b4267238b7
pkhateri <parisa.khaterigmail.com>
2023-10-20 13:19:34 +0200

    dataloader: clean the code

--- 2D/dataloader.py
+++ 2D/dataloader.py
@@ -3,7 +3,7 @@ import numpy as np
 import os
 import json
 import settings
-    
+
 def get_decathlon_filelist(data_path, seed=816, split=0.85):
     """
     Get the paths for the original decathlon files
@@ -41,7 +41,7 @@ def get_decathlon_filelist(data_path, seed=816, split=0.85):
     numFiles = experiment_data["numTraining"]
     idxList = np.arange(numFiles)  # List of file indices
     np.random.shuffle(idxList) # Shuffle the indices to randomize train/test/split
-    
+
     trainIdx = int(np.floor(numFiles*split)) # index for the end of the training files
     trainList = idxList[:trainIdx]
 
@@ -74,31 +74,31 @@ class DatasetGenerator(Sequence):
     """
     TensorFlow Dataset from Python/NumPy Iterator
     """
-    
+
     def __init__(self, filenames, batch_size=8, crop_dim=[240,240], augment=False, seed=816):
-        
+
         import nibabel as nib
 
         img = np.array(nib.load(filenames[0]).dataobj) # Load the first image
         self.slice_dim = 2  # We'll assume z-dimension (slice) is last
         # Determine the number of slices (we'll assume this is consistent for the other images)
-        self.num_slices_per_scan = img.shape[self.slice_dim]  
+        self.num_slices_per_scan = img.shape[self.slice_dim]
 
         # If crop_dim == -1, then don't crop
         if crop_dim[0] == -1:
             crop_dim[0] = img.shape[0]
         if crop_dim[1] == -1:
             crop_dim[1] = img.shape[1]
-        self.crop_dim = crop_dim  
+        self.crop_dim = crop_dim
 
         self.filenames = filenames
         self.batch_size = batch_size
 
         self.augment = augment
         self.seed = seed
-        
+
         self.num_files = len(self.filenames)
-        
+
         self.ds = self.get_dataset()
 
     def preprocess_img(self, img):
@@ -110,19 +110,19 @@ class DatasetGenerator(Sequence):
 
     def preprocess_label(self, label):
         """
-        Predict whole tumor. If you want to predict tumor sections, then 
+        Predict whole tumor. If you want to predict tumor sections, then
         just comment this out.
         """
         label[label > 0] = 1.0
 
         return label
-    
+
     def augment_data(self, img, msk):
         """
         Data augmentation
         Flip image and mask. Rotate image and mask.
         """
-        
+
         if np.random.rand() > 0.5:
             ax = np.random.choice([0,1])
             img = np.flip(img, ax)
@@ -174,10 +174,17 @@ class DatasetGenerator(Sequence):
         Python generator which goes through a list of filenames to load.
         The files are 3D image (slice is dimension index 2 by default). However,
         we need to yield them as a batch of 2D slices. This generator
-        keeps yielding a batch of 2D slices at a time until the 3D image is 
+        keeps yielding a batch of 2D slices at a time until the 3D image is
         complete and then moves to the next 3D image in the filenames.
-        An optional `randomize_slices` allows the user to randomize the 3D image 
+        An optional `randomize_slices` allows the user to randomize the 3D image
         slices after loading if desired.
+
+        idx: to track the number of 3d images which have been read so far. idx[0,num_filenames]
+        idy: number of 2d images which have been read so far.
+            If after reading one 3d image, this number is less than the batch size,
+            we continue reading the next 3D image. idy[0,batch_size]
+        idz: index for adding 3d images (files) to the queue. idz[0,1+batch_size//num_slices_per_scan]
+
         """
         import nibabel as nib
 
@@ -192,21 +199,21 @@ class DatasetGenerator(Sequence):
             Pack N_IMAGES files at a time to queue
             """
             NUM_QUEUED_IMAGES = 1 + self.batch_size // self.num_slices_per_scan  # Get enough for full batch + 1
-            
+
             for idz in range(NUM_QUEUED_IMAGES):
 
                 label_filename = self.filenames[idx]
 
                 #img_filename   = label_filename.replace("_seg.nii.gz", "_flair.nii.gz") # BraTS 2018
                 img_filename   = label_filename.replace("labelsTr", "imagesTr")  # Medical Decathlon
-                
+
                 img = np.array(nib.load(img_filename).dataobj)
                 img = img[:,:,:,0]  # Just take FLAIR channel (channel 0)
                 img = self.preprocess_img(img)
 
                 label = np.array(nib.load(label_filename).dataobj)
                 label = self.preprocess_label(label)
-                
+
                 # Crop input and label
                 img, label = self.crop_input(img, label)
 
@@ -215,20 +222,19 @@ class DatasetGenerator(Sequence):
                     label_stack = label
 
                 else:
-
                     img_stack = np.concatenate((img_stack,img), axis=self.slice_dim)
                     label_stack = np.concatenate((label_stack,label), axis=self.slice_dim)
-                
-                idx += 1 
+
+                idx += 1
                 if idx >= len(self.filenames):
                     idx = 0
                     np.random.shuffle(self.filenames) # Shuffle the filenames for the next iteration
-            
+
             img = img_stack
             label = label_stack
 
             num_slices = img.shape[self.slice_dim]
-            
+
             if self.batch_size > num_slices:
                 raise Exception("Batch size {} is greater than"
                                 " the number of slices in the image {}."
@@ -244,23 +250,19 @@ class DatasetGenerator(Sequence):
                 img = img[:,:,slice_idx]  # Randomize the slices
                 label = label[:,:,slice_idx]
 
-            name = self.filenames[idx]
-            
             if (idy + self.batch_size) < num_slices:  # We have enough slices for batch
-                img_batch, label_batch = img[:,:,idy:idy+self.batch_size], label[:,:,idy:idy+self.batch_size]   
-
+                img_batch, label_batch = img[:,:,idy:idy+self.batch_size], label[:,:,idy:idy+self.batch_size]
             else:  # We need to pad the batch with slices
-
                 img_batch, label_batch = img[:,:,-self.batch_size:], label[:,:,-self.batch_size:]  # Get remaining slices
 
             if self.augment:
                 img_batch, label_batch = self.augment_data(img_batch, label_batch)
-                
+
             if len(np.shape(img_batch)) == 3:
                 img_batch = np.expand_dims(img_batch, axis=-1)
             if len(np.shape(label_batch)) == 3:
                 label_batch = np.expand_dims(label_batch, axis=-1)
-                
+
             yield np.transpose(img_batch, [2,0,1,3]).astype(np.float32), np.transpose(label_batch, [2,0,1,3]).astype(np.float32)
 
 
@@ -272,46 +274,46 @@ class DatasetGenerator(Sequence):
             if idx >= len(self.filenames):
                 idx = 0
                 np.random.shuffle(self.filenames) # Shuffle the filenames for the next iteration
-                
+
 
     def get_input_shape(self):
         """
         Get image shape
         """
         return [self.crop_dim[0], self.crop_dim[1], 1]
-        
+
     def get_output_shape(self):
         """
         Get label shape
         """
-        return [self.crop_dim[0], self.crop_dim[1], 1] 
-    
+        return [self.crop_dim[0], self.crop_dim[1], 1]
+
     def get_dataset(self):
         """
         Return a dataset
         """
         ds = self.generate_batch_from_files()
-        
-        return ds  
-    
+
+        return ds
+
     def __len__(self):
         return (self.num_slices_per_scan * self.num_files)//self.batch_size
 
     def __getitem__(self, idx):
         return next(self.ds)
-        
+
     def plot_samples(self):
         """
         Plot some random samples
         """
         import matplotlib.pyplot as plt
-        
+
         img, label = next(self.ds)
-        
+
         print(img.shape)
- 
+
         plt.figure(figsize=(10,10))
-        
+
         slice_num = 3
         plt.subplot(2,2,1)
         plt.imshow(img[slice_num,:,:,0]);
